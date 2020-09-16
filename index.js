9dabdac66351c84dacaf67957cf7c59040f27c95
timqian <timqian92@qq.com>
2020-09-16 12:19:28 +0800

    init

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const issue = require('./utils/issue');
 
 // run every day at 00:01 UTC
 const run = async (date) => {
-  const contents = await getHeadlines(date);
+  // const contents = await getHeadlines(date);
   console.log(contents)
   const res = await issue.open({
     owner: 'headllines',
-    repo: 'hackernews-daily',
-    title: `Hacker News Daily Top 10 @${new Date(date).toISOString().slice(0, 10)}`,
-    body: contents
+    repo: 'github-daily',
+    title: `GitHub Daily Top 10 @${new Date(date).toISOString().slice(0, 10)}`,
+    body: 'TODO'
   });
 
   const issueNumber = res.data.number;
 
   await issue.lock({
     owner: 'headllines',
-    repo: 'hackernews-daily', 
+    repo: 'github-daily', 
     issueNumber,
   });
 }
