81219c8829d14afe8f437a0c2b674a7dde5ea532
timqian <timqian92@qq.com>
2020-10-03 00:17:32 +0800

    init

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const issue = require('./utils/issue');
 
 // run every day at 00:01 UTC
 const run = async (date) => {
-  // const contents = await getHeadlines(date);
-  console.log(contents)
+  const contents = await getHeadlines(date);
+  // console.log(contents)
   const res = await issue.open({
     owner: 'headllines',
     repo: 'github-daily',
     title: `GitHub Daily Top 10 @${new Date(date).toISOString().slice(0, 10)}`,
-    body: 'TODO'
+    body: contents,
   });
 
   const issueNumber = res.data.number;
--- utils/getHeadlines.js
+++ utils/getHeadlines.js
@@ -1,27 +1,21 @@
 const axios = require('axios');
-const timeago = require('timeago.js');
 
-const getHeadlines = async (date) => {
+const getHeadlines = async () => {
   console.log('start fetching headlines')
   try {
-    // end of the date
-    const endTime = Math.round(new Date(date).getTime() / 1000);
-    // 1 hour before start of the date (save missed posts)
-    const startTime = Math.round(new Date(date).getTime() / 1000) - (25 * 60 * 60);
-    const res = await axios.get(`https://hn.algolia.com/api/v1/search?numericFilters=created_at_i>${startTime},created_at_i<${endTime}`);
-    const top10Objs = res.data.hits.slice(0, 10);
+    const res = await axios.get(`https://ghapi.huchen.dev/repositories?&since=daily`);
+    const top10Objs = res.data.slice(0, 10);
     // console.log(top10Objs)
     const contents = top10Objs
       .map((obj, i) => {
-        let { title, created_at, url, author, points, objectID, num_comments } = obj;
-        if(!url) url = `https://news.ycombinator.com/item?id=${objectID}`;
+        let { author, name, url, description, stars, forks, currentPeriodStars, language} = obj;
 
-        return `${i + 1}. **[${title}](${url})**
-${points} points by [${author}](https://news.ycombinator.com/user?id=${author}) ${timeago.format(created_at)} | [${num_comments} comments](https://news.ycombinator.com/item?id=${objectID})
+        return `${i + 1}. [**${author}/${name}**: ${description}](${url})
+${currentPeriodStars} stars today | ${stars} stars | ${forks} forks ${language ? '| ' + language : ''}
 
 `;
       }).join('');
-
+    // console.log(contents);
     return contents;
   } catch (error) {
     console.log(error);
