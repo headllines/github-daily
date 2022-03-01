7605298351c371a3079f76305287b94d64a36d60
Tim Qian <timqian@t9t.io>
2022-03-01 08:56:59 +0000

    Use https://github.com/xxdongs/github-trending

--- utils/getHeadlines.js
+++ utils/getHeadlines.js
@@ -3,19 +3,20 @@ const axios = require('axios');
 const getHeadlines = async () => {
   console.log('start fetching headlines')
   try {
-    const res = await axios.get(`https://github-trending-api.waningflow.com/repositories?since=daily`);
-    const top10Objs = res.data.slice(0, 10);
+    const res = await axios.get(`https://trendings.herokuapp.com/repo?since=daily`);
+    // console.log(res.data.items)
+    const top10Objs = res.data.items.slice(0, 10);
     // console.log(top10Objs)
     const contents = top10Objs
       .map((obj, i) => {
-        let { author, name, url, description, stars, forks, currentPeriodStars, language} = obj;
+        let { repo, repo_link, desc, stars, forks, added_stars, lang} = obj;
 
-        return `${i + 1}. [**${author}/${name}**${description ? ': ' + description : ''}](${url})
-${currentPeriodStars} stars today | ${stars} stars | ${forks} forks ${language ? '| ' + language : ''}
+        return `${i + 1}. [**${repo}**${desc ? ': ' + desc : ''}](${repo_link})
+${added_stars} stars today | ${stars} stars | ${forks} forks ${lang ? '| ' + lang : ''}
 
 `;
       }).join('');
-    // console.log(contents);
+    console.log(contents);
     return contents;
   } catch (error) {
     console.log(error);
